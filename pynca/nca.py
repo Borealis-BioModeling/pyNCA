65656cac4193221d1e09a3512083f45136c7aff7
blakeaw <blakeaw1102@gmail.com>
2025-03-25 17:05:05 -0500

    Added the terminal_rate function.

--- pynca/nca.py
+++ pynca/nca.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numba
+from scipy.stats import linregress
+
 
 @numba.njit(cache=True)
 def area_under_curve(t: np.array, c: np.array) -> float:
@@ -19,6 +21,7 @@ def area_under_curve(t: np.array, c: np.array) -> float:
     dt = t[1] - t[0]
     return np.trapz(c, t, dx=dt)
 
+
 @numba.njit(cache=True)
 def cmax_tmax(t: np.array, c: np.array) -> tuple[float, float]:
     """Returns the maximum concentration and time of maximum concentration.
@@ -34,3 +37,26 @@ def cmax_tmax(t: np.array, c: np.array) -> tuple[float, float]:
     cmax = c[cmax_idx]
     tmax = t[cmax_idx]
     return (cmax, tmax)
+
+
+@numba.njit(cache=True)
+def terminal_rate(t: np.array, c: np.array, n_points: int) -> tuple[float, float, float, float]:
+    """Returns an estimate of the terminal rate constant and half-life.
+
+    Estimates the terminal rate constant and terminal half-life using
+    the specified number of points from the terminal portion of the
+    log(concentration)-rate curve.
+
+    Args:
+        t (np.array): Times.
+        c (np.array): Concentrations.
+        n_points (int): Number of terminal points.
+
+    Returns:
+        tuple[float, float]: rate_constant, t_half, r_squared, adjusted_r_squared
+    """
+    slope, intercept, r, p, se = linregress(t, np.log(c))
+    r_squared = r**2
+    adjusted_r_squared = 1 - (1 - r_squared) * (n_points - 1) / (n_points - 2)
+    t_half = np.log(2) / slope
+    return (slope, t_half, r_squared, adjusted_r_squared)
